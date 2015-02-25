1356f24e29e84b80b30987a78e8d7fde9efc1251
Jeff-Russ <jeffreylynnruss@gmail.com>
2015-02-25 18:24:57 -0500

    updated introjucer with new source

--- JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
+++ JuceLibraryCode/modules/juce_audio_plugin_client/juce_audio_plugin_client.h
@@ -1,5 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../../../usr/include/juce/modules/juce_audio_basics/juce_audio_basics.h"
+#include "../../../../JUCE/modules/juce_audio_plugin_client/juce_audio_plugin_client.h"
 
--- JuceLibraryCode/modules/juce_gui_extra/juce_gui_extra.h
+++ JuceLibraryCode/modules/juce_gui_extra/juce_gui_extra.h
@@ -1,5 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#include "../../../../../../../usr/include/juce/modules/juce_gui_extra/juce_gui_extra.h"
+#include "../../../../JUCE/modules/juce_gui_extra/juce_gui_extra.h"
 
