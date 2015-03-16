9eff77330c0f993c314202cc8313e522da06130b
Jeff-Russ <jeffreylynnruss@gmail.com>
2015-03-16 11:44:42 -0400

    Adding  command line testing runner

---
+++ Builds/MacOSX/jATK-CL/jATK-CL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  jATK-CL
+//
+//  Created by jefrus on 3/16/15.
+//  Copyright (c) 2015 Jeff-Russ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
