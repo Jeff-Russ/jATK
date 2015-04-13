34a8b0b2b0dcca895dacb35401770372fed8153c
Jeff-Russ <jeffreylynnruss@gmail.com>
2015-04-13 00:41:25 -0400

    added more to AHDSR

--- Source/jATK/AHDSR.h
+++ Source/jATK/AHDSR.h
@@ -15,8 +15,8 @@ namespace jATK
 {
     class AHDSR // analog capacitor based envelope emulation from Reaktor
     {           // original module "AHDSR Capacitor V3.4"
-        Audio sr, a, h, d, s = 0, r, vc, vin, tau, z = 0;
-        int rtrig;
+        Audio sr, a, h = 0, d, s = 0, r, vc, vin, tau, z = 0, ssA;
+        int rm, sI, ssI;
     public:
         AHDSR (Audio sRate) { sr = sRate; }
         void set (Audio attack, Audio hold, Audio decay, Audio sustain,
@@ -27,28 +27,45 @@ namespace jATK
         }
         void setSRate (Audio sRate)  { sr = sRate; }
         void attack (Audio seconds) { a = seconds; }
-        void hold (Audio seconds) { h = seconds; }
+        void hold (Audio seconds) { h = seconds;  }
         void decay (Audio seconds) { d = clipMin (seconds, Audio(1E-012)); }
         void sustain (Audio zeroTo1) { s = clipMinMax (zeroTo1, ZERO_A); }
         void release (Audio seconds) { r = clipMin (seconds, Audio (1E-012)); }
-        void retrig (int zeroTo3) { rtrig = clipMinMax(zeroTo3, 0, 3); }
+        void retrig (int zeroTo3) { rm = clipMinMax(zeroTo3, 0, 3); }
         Audio on (){}
         Audio off (){}
 
     private:
+        Audio a1, a2, a3;
+        
         Audio rcEnv()
-        {   Audio a1 = pow ( 2.71828f, 1 / tau * sr * -5.f );
-            Audio a2 = vin * (1 - a1);
-            Audio a3 = a1 * z;
+        {   a1 = pow ( 2.71828f, 1 / tau * sr * -5.f );
+            a2 = vin * (1 - a1);
+            a3 = a1 * z;
             z = a2 + a3;
             return z;
         }
-
-
+        void gate(Audio GVel)
+        {   if (GVel)
+            {   if (  ((rm & 1) == 0)  ||  (sI == 0)   )
+                vin = GVel;
+                this->attackHold();
+            }else { this->release(); }
+        }
+        void attackHold()
+        {   ssA = 0.f; ssI = 0;
+            tau = (1 - z / vin) * a;
+            incr = 1 / ((tau + h) * sr)
+            
+        }
+        void delaySustain()
+        {
+            
+        }
+        void release()
+        {
+            
+        }
     };
-
-
-
 } // end namespace jATK
-
 #endif  // AHDSR_H_INCLUDED
